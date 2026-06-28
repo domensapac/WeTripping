8e9bf74952bbf962c37759f1d2fb003a5eb6ab30
domen <domen.sapac10@gmail.com>
2026-06-28 16:02:16 +0200

    SignIn

---
+++ app/login/page.tsx
@@ -0,0 +1,58 @@
+import { Architects_Daughter } from "next/font/google";
+
+const architectsDaughter = Architects_Daughter({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-architects',
+});
+
+export default function Login() {
+  return (
+    <div className="tracking-[1px] flex min-h-screen w-full text-gray-400">
+        <div className="hidden md:flex md:w-1/3 lg:w-1/2 bg-cover bg-[url('/cat_back.png')] ...">    
+        </div>
+        <div className="bg-cover bg-[url('/cat_back.png')] md:bg-none w-full md:w-2/3 lg:w-1/2 flex justify-center items-center">
+            <div className="bg-white md:bg-none w-full flex-1 m-6 md:m-12 border-1 border-white rounded-lg flex-col">
+                <span className={`flex text-xl w-full justify-center text-gray-400 ${architectsDaughter.className}`}> Welcome back! </span>
+                <span className="flex text-3xl p-3 mb-8 w-full justify-center text-gray-600"> Sign In </span>
+                <div className="flex justify-center items-center ">
+                    <span className="hover:bg-[#F9EFE1] hover:border-gray-300 border-1 flex rounded-sm p-1 hover:cursor-pointer items-center me-1"> 
+                        <svg className="p-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="18px" height="18px">
+                            <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"/>
+                            <path fill="#4285F4" d="M46.5 24c0-1.61-.15-3.16-.42-4.69H24v9.09h12.75c-.55 2.87-2.16 5.31-4.6 6.96l7.19 5.57C43.53 36.31 46.5 30.73 46.5 24z"/>
+                            <path fill="#FBBC05" d="M10.54 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.98-6.19z"/>
+                            <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.19-5.57c-1.99 1.33-4.55 2.13-7.7 2.13-6.26 0-11.57-4.22-13.46-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"/>
+                        </svg>
+                        Google 
+                    </span>
+                    <span className="hover:bg-[#F9EFE1] hover:border-gray-300 border-1 flex rounded-sm p-1 hover:cursor-pointer items-center ms-1"> 
+                        <svg className="p-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="18px" height="18px">
+                            <path fill="#1877F2" d="M48,24A24,24,0,1,0,20.25,47.75V31H14.13V24h6.12V18.62c0-6,3.58-9.38,9.11-9.38a37.07,37.07,0,0,1,5.4.47v5.94H31.73c-3,0-3.89,1.86-3.89,3.77V24h6.69l-1.07,7H27.84V47.75A24,24,0,0,0,48,24Z"/>
+                        </svg>
+                        Facebook 
+                    </span>
+                </div>
+                <div className="flex items-center my-6 w-2/3 md:w-1/2 mx-auto text-gray-300">
+                    <div className="flex-1 border-t border-gray-300"></div>
+                    <span className="px-3 text-sm text-gray-400">OR</span>
+                    <div className="flex-1 border-t border-gray-300"></div>
+                </div>
+                <div className="flex flex-col justify-center items-center m-7">
+                    <div className="w-2/3 md:w-1/2 flex flex-col mb-1">
+                        <label>Email</label>
+                        <input className="border-1 rounded-md p-1 text-gray-600" id="email" name="email" type="text"/>
+                    </div>
+                    <div className="w-2/3 md:w-1/2 flex flex-col mt-1">
+                        <label>Password</label>
+                        <input className="border-1 rounded-md p-1 text-gray-600" id="password" name="password" type="password"/>
+                    </div>
+                    <button className="text-gray-600 bg-[#F9EFE1] border-gray-300 hover:cursor-pointer mt-4 px-2 py-1 border-1 rounded-sm" type="submit">
+                        Sign In
+                    </button>
+                    <span className="my-4">Don't have an account? <span className="hover:cursor-pointer text-gray-600">Sign up</span></span>
+                </div>
+            </div>
+        </div>
+    </div>
+      );
+}
