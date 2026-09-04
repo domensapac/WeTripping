26161ccdea6cc36d4465bec96fa544e812b0b274
domen <domen.sapac10@gmail.com>
2026-09-04 15:07:32 +0200

    Trying

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -11,7 +11,7 @@ export default function AppLayout({
 }>) {
 
   return (
-     <div className="flex flex-col w-full h-screen">
+     <div className="flex flex-col w-full h-dvh">
       <div className="w-full flex flex-1 min-h-0 overflow-y-auto">
         {children}
       </div>
